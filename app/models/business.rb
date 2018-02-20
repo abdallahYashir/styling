0304062617ed578811a843d8a7e89395af3f2977
Abdallah Yashir <abdallah.yashir@chb-technologies.ch>
2018-02-20 17:13:03 +0400

    Improve navbar + layout

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,4 +1,4 @@
 class Business < ApplicationRecord
-  belongs_to :type
+  belongs_to :type, optional: true
   validates :name, :address1, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# businessType = Type.create(name: 'Barber', description: 'Barber')
+#
+# Type.create(name: 'Hair Stylist', description: 'Hair Stylist')
+
+puts "Creating Business Types"
+businessType = Type.create!(name: 'Barber', description: 'Barber')
+
+puts "Creating Business"
+
+business = Business.create!(name: 'Acktar Coiffure', description: 'By Acktar', active: true, address1: 'Port Louis', type_id: businessType)
+
+
+puts "Creating Days"
+Day.create([
+  { name: 'Monday' },
+  { name: 'Tuesday' },
+  { name: 'Wednesday' },
+  { name: 'Thursday' },
+  { name: 'Friday' },
+  { name: 'Saturday' },
+  { name: 'Sunday' },
+  { name: 'Holiday' },
+])
+
+puts "Creating Service"
+Service.create!([
+  { name: 'Haircut', description: 'Fresh & Trendy Haircut', price: 125, business_id: business },
+  { name: 'Beard Trim', description: 'Beard Trimming', price: 100, business_id: business },
+  { name: 'Shampoing', description: 'Chill Shampoing', price: 150, business_id: business }
+])
+
+puts "Creating Image Type"
+ImageType.create!([
+  { name: 'Profile' },
+  { name: 'Background' },
+  { name: 'Portfolio' }
+])
